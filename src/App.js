638dc0b98fc68546e1a7401294b672fe44f2ca5b
Ziad Tarek <ziadtarekfa@gmail.com>
2022-11-21 22:47:18 +0200

    implemented getLocation using ip address

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import arrowIcon from './icons/icon-arrow.svg';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const APIKEY = "at_Liuec0O6i0zNUKMJUryMa5FscrYtg";
+  const getLocation = (ip) => {
+
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=${APIKEY}&ipAddress=${ip}`)
+      .then((response) => {
+        return response.json()
+      }).then((data) => {
+        console.log(data);
+      });
+  }
+  return (
+    <main className="App">
+      <div className='search-ip-container'>
+        <h1>IP Address Tracker</h1>
+        <div>
+          <input placeholder='Search for any IP Address'></input>
+          <button onClick={() => getLocation("192.161.2.1")}>Click me</button>
+          {/* <img src={arrowIcon} alt="arrow icon"></img> */}
+        </div>
+      </div>
+      <div className='map'>
+
+      </div>
+    </main>
   );
 }
+// at_Liuec0O6i0zNUKMJUryMa5FscrYtg
 
 export default App;
