47ad99231878da857ba4f65551076406cf3588b5
Ziad Tarek <ziadtarekfa@gmail.com>
2022-11-25 23:29:06 +0200

    finished map functionalities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents, useMapEvent } from 'react-leaflet'
-import Map from './components/Map';
 import './App.css';
 
 import arrowIcon from './icons/icon-arrow.svg';
@@ -8,44 +7,52 @@ import arrowIcon from './icons/icon-arrow.svg';
 
 
 
+
 function App() {
 
-  // let myposition = [20.2, 20.1];
-  // function Map() {
-
-  //   const map = useMap();
-  //   useEffect(() => {
-  //     console.log("LOADING BITCH");
-  //     if ('geolocation' in navigator) {
-  //       /* geolocation is available */
-  //       navigator.geolocation.getCurrentPosition((position) => {
-  //         map.flyTo([position.coords.latitude, position.coords.longitude], 12);
-  //       //  myposition = [position.coords.latitude, position.coords.longitude]
-  //         //  setPosition([position.coords.latitude, position.coords.longitude]);
-  //       });
-  //     }
-  //   }, []);
 
 
-  // }
+  const mapRef = useRef();
 
+
+  const [markerPosition, setMarkerPosition] = useState([38.685516, -101.073324]);
+
+  useEffect(() => {
+
+    if ('geolocation' in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { current: map } = mapRef;
+        map.flyTo([position.coords.latitude, position.coords.longitude], 12);
+        setMarkerPosition([position.coords.latitude, position.coords.longitude]);
+      });
+    }
+  }, []);
+
+  function handleClick() {
+    const { current: map } = mapRef;
+    map.flyTo([20.3, -81.5], 12);
+    setMarkerPosition([20.3, -81.5]);
+  }
   return (
     <main>
 
-
-      <MapContainer
+      <MapContainer ref={mapRef}
         className='map'
         center={[38.685516, -101.073324]}
         zoom={6}
         scrollWheelZoom={true}
+
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Marker position={markerPosition}></Marker>
 
-        <Map />
       </MapContainer>
+
+      <button onClick={handleClick}>CLICK ME</button>
 
     </main>
 
