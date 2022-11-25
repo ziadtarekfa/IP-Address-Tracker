bcc38f7f7c5a228e06ae08ff1d6dc2ce2b5210d0
Ziad Tarek <ziadtarekfa@gmail.com>
2022-11-25 14:34:55 +0200

    added marker onload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents, useMapEvent } from 'react-leaflet'
+import Map from './components/Map';
 import './App.css';
 
 import arrowIcon from './icons/icon-arrow.svg';
 
-function Map() {
 
-  const map = useMap();
-  useEffect(() => {
-    console.log("LOADING BITCH");
-    if ('geolocation' in navigator) {
-      /* geolocation is available */
-      navigator.geolocation.getCurrentPosition((position) => {
-        map.flyTo([position.coords.latitude, position.coords.longitude], 12);
-      });
-    }
-  }, []);
-}
+
 
 function App() {
+
+  // let myposition = [20.2, 20.1];
+  // function Map() {
+
+  //   const map = useMap();
+  //   useEffect(() => {
+  //     console.log("LOADING BITCH");
+  //     if ('geolocation' in navigator) {
+  //       /* geolocation is available */
+  //       navigator.geolocation.getCurrentPosition((position) => {
+  //         map.flyTo([position.coords.latitude, position.coords.longitude], 12);
+  //       //  myposition = [position.coords.latitude, position.coords.longitude]
+  //         //  setPosition([position.coords.latitude, position.coords.longitude]);
+  //       });
+  //     }
+  //   }, []);
+
+
+  // }
 
   return (
     <main>
@@ -29,7 +38,6 @@ function App() {
         center={[38.685516, -101.073324]}
         zoom={6}
         scrollWheelZoom={true}
-
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
