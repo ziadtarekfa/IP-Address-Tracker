107c28cadc5d1a931fa57b94d426f7f9307fb2de
Ziad Tarek <ziadtarekfa@gmail.com>
2022-11-26 19:18:31 +0200

    implemented display ip details functionality

--- src/App.js
+++ src/App.js
@@ -3,37 +3,85 @@ import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents, useMapEve
 import './App.css';
 import IpResult from './components/IpResult';
 
-import arrowIcon from './icons/icon-arrow.svg';
-
-
-
-
 
 function App() {
 
-
-
   const mapRef = useRef();
-
-
-  const [markerPosition, setMarkerPosition] = useState([38.685516, -101.073324]);
+  const ipInputRef = useRef();
 
   useEffect(() => {
+
+    fetch("https://geolocation-db.com/json/").then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      setIpResult({
+        ip: data.IPv4,
+        ipLocation: data.country_name,
+        timezone: "GMT:05:00",
+        isp: "TE DATA"
+      })
+    })
 
     if ('geolocation' in navigator) {
       /* geolocation is available */
       navigator.geolocation.getCurrentPosition((position) => {
         const { current: map } = mapRef;
+        console.log(position);
         map.flyTo([position.coords.latitude, position.coords.longitude], 12);
         setMarkerPosition([position.coords.latitude, position.coords.longitude]);
       });
     }
   }, []);
 
+  const [markerPosition, setMarkerPosition] = useState([38.685516, -101.073324]);
+
+
+  const [ipResult, setIpResult] = useState({
+    ip: "192.17.2.1",
+    ipLocation: "Brooklyn, NY 10001",
+    timezone: "UTC-05:00",
+    isp: "SpaceX Starlink"
+  });
+
+
   function handleClick() {
     const { current: map } = mapRef;
-    map.flyTo([20.3, -81.5], 12);
-    setMarkerPosition([20.3, -81.5]);
+
+    if (ipInputRef.current.value !== "") {
+      fetch(`https://api.ipbase.com/v2/info?ip=${ipInputRef.current.value}&apikey=olN7DnIT9RNM0SBtUAKGl1cQdau13huJhOcpLMzI`)
+        .then((response) => {
+          return response.json();
+        }).then((res) => {
+          if (res.message) {
+            console.log("Invalid IP Address");
+          }
+          else {
+            const location = res.data.location;
+
+            setIpResult({
+
+              ip: res.data.ip,
+              ipLocation: location.city.name_translated + "," + location.country.name,
+              timezone: res.data.timezone.code + "-" + res.data.timezone.current_time,
+              isp: res.data.connection.isp
+
+            });
+            console.log(res.data);
+            map.flyTo([location.latitude, location.longitude], 12);
+            setMarkerPosition([location.latitude, location.longitude]);
+
+
+          }
+
+        });
+      console.log(ipResult);
+    }
+    else {
+      console.log("Please enter an ip address");
+    }
+
+
   }
   return (
     <>
@@ -42,7 +90,7 @@ function App() {
         <div className='container'>
           <h2>IP Address Tracker</h2>
           <div className='ip-input-container'>
-            <input placeholder='Search for any IP address'></input>
+            <input placeholder='Search for any IP address' ref={ipInputRef}></input>
             <button onClick={handleClick}>Click me</button>
           </div>
 
@@ -66,7 +114,7 @@ function App() {
 
 
       </main>
-      <IpResult />
+      <IpResult myprop={ipResult} />
     </>
 
   );
--- src/components/IpResult.js
+++ src/components/IpResult.js
@@ -1,30 +1,34 @@
+import { useEffect } from 'react';
 import '../IpResult.css';
 
-const IpResult = () => {
+const IpResult = ({ myprop }) => {
+
+
     return (
         <div className="ip-result">
 
             <div className="ip-address-container">
                 <h5>IP ADDRESS</h5>
-                <h2>192.212.174.101</h2>
+                <h2>{myprop.ip}</h2>
+
             </div>
             <hr />
             <div className="location-container">
                 <h5>LOCATION</h5>
-                <h2>Brooklyn, NY 10001 </h2>
+                <h2>{myprop.ipLocation} </h2>
 
             </div>
             <hr />
 
             <div className="timezone-container">
                 <h5>TIMEZONE</h5>
-                <h2>UTC-05:00</h2>
+                <h2>{myprop.timezone}</h2>
             </div>
             <hr />
 
             <div className="isp-container">
                 <h5>ISP</h5>
-                <h2>SpaceX Starlink</h2>
+                <h2>{myprop.isp}</h2>
             </div>
 
         </div>
